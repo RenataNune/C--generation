fb6f7356b601b44a14bdd90341155c191754e359
RenataNune <renatan.sil97@gmail.com>
2022-04-06 17:27:06 -0300

    adicionando lista6 C#

--- Aula6VS/Aula6VS/Program.cs
+++ Aula6VS/Aula6VS/Program.cs
@@ -4,17 +4,19 @@ namespace Aula6VS
 {
     internal class Program
     {
+        private const int V = 3;
+
         static void Main(string[] args)
         {
-            int valor = 0;
+            Console
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < V; i++)
             {
                 Console.WriteLine("entre com o valor deseja somar:");
                 valor += int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine($"Somatoria: {valor}");
+                Console.WriteLine($"Somatoria: {valor}");
 
         }
 
---
+++ Lista6-C#/Lista6-C/Lista6-C/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabuada
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("qual tabuada você gotaria de ver? ");
+            int tab = Convert.ToInt16(Console.ReadLine());
+            for (int e = 1; e <= 10; e++)
+            {
+                for (int c = 1; c <= 10; c++)
+                {
+                    Console.WriteLine(tab + " x " + c + " = " + tab * c);
+                }
+
+
+                Console.Read();
+            }
+
+        }
+    }
+}
