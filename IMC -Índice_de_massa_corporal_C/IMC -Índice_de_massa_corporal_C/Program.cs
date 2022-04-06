2ed46e281e494781260fc4e8d0fda1c07651af23
RenataNune <renatan.sil97@gmail.com>
2022-04-05 23:52:27 -0300

    Exercicios C#

---
+++ IMC -Índice_de_massa_corporal_C/IMC -Índice_de_massa_corporal_C/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IMC__Índice_de_massa_corporal_C
+{
+
+    internal class IMClesson
+    {
+        static void Main(string[] args)
+        {
+            float altura, peso, imc;
+
+            Console.WriteLine("Qual o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Olá" + nome + ". Agora me diga sua altura por favor: ");
+            altura = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Perfeito. Agora me diga seu peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            imc = peso / (altura * altura);
+
+            Console.WriteLine("Seu IMC é: " + imc);
+
+        }
+    }
+}
+
+
+
+
---
+++ IMC-Índice_de_massa _corporal_C/IMC-Índice_de_massa _corporal_C/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Projimc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float altura, peso, imc;
+
+            Console.WriteLine("Qual seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Olá " + nome + "!" + "Vamos calcular seu IMC! Qual o seu peso? ");
+            peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a sua altura em metros?");
+            altura = float.Parse(Console.ReadLine());
+
+
+            imc = peso / (altura * altura);
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Seu imc é: " + imc + ". " + "Abaixo do peso recomendado. ");
+            }
+            else if (imc < 25)
+            {
+                Console.WriteLine("Seu imc é: " + imc + ". " + "Peso normal. ");
+            }
+            else if (imc < 30)
+            {
+                Console.WriteLine("Seu imc é: " + imc + "." + "Sobrepeso. ");
+            }
+            else if (imc >= 30)
+            {
+                Console.WriteLine("Seu imc é: " + imc + ". " + "Obesidade.");
+            }
+
+        }
+    }
+}
+
