9540e35adc958dc242af2d69347a5e068afaa8bf
RenataNune <renatan.sil97@gmail.com>
2022-04-06 12:06:02 -0300

    main

---
+++ Aula6VS/Aula6VS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula6VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("entre com o valor deseja somar:");
+                valor += int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Somatoria: {valor}");
+
+        }
+
+         
+ 
+    }
+}
