9ee49d19d0fe9b8b34feca2be7944ea9f78412a3
RenataNune <renatan.sil97@gmail.com>
2022-04-13 10:36:46 -0300

    adicionar pasta

---
+++ Lista10-C#-Polimorfismo/List10-Polimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace List10_Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
